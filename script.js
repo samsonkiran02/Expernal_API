17d568cff629c086f78a3a060ee66f09e2fceb04
samsonkiran02 <www.samsonkiran02@g.com>
2024-11-26 18:32:05 +0530

    Data getted successfully

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ const LoadAPI = () => {
     .then(data => console.log(data))
 }
 
+// get data
+const Getdata = (product) => {
+    return (`
+        <div class="product">
+            <img src="${product.image}">
+            <p>${product.title}</p>
+            <p>${product.category}</p>
+            <p>${product.price}</p>
+        </div>
+    `)
+}
+
 LoadAPI();
